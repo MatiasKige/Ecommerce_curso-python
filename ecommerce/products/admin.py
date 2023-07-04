b35e2c458116777193667bd57853a178fc2fd561
Matias Gigena <mati-mangekyou@hotmail.com>
2023-07-04 20:23:10 -0300

    Correcciones y puesta al dia

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -3,4 +3,10 @@ from products.models import Products
 
 # Register your models here.
 
-admin.site.register(Products)
+# admin.site.register(Products)
+
+#       Otra manera de registrar un modelo:
+
+@admin.register(Products)
+class Products_admin(admin.ModelAdmin):
+    list_display=["name","price","stock","is_active"]
