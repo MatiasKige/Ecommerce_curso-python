c7ceb73a005385b501b8285d2698d8ae5eab3a5e
Matias Gigena <mati-mangekyou@hotmail.com>
2023-07-25 20:07:52 -0300

    Segundo articuloDetalles creado con CBV

--- ecommerce/blog/urls.py
+++ ecommerce/blog/urls.py
@@ -4,5 +4,5 @@ from blog.views import create_article, List_articles, Detail_article
 urlpatterns = [
     path("create-article/",create_article,name="create_article"),
     path("list-articles/",List_articles.as_view(),name="List_articles"),
-    path("detail-article/",Detail_article.as_view(),name="Detail_article")
+    path("detail-article/<int:pk>/",Detail_article.as_view(),name="Detail_article")
 ]
