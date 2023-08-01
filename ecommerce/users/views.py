54f3ef2954b300aeedd774797aa3932e6c5d18c2
Matias Gigena <mati-mangekyou@hotmail.com>
2023-08-01 20:51:19 -0300

    Login creado

--- ecommerce/users/views.py
+++ ecommerce/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                context = {"message":f"Bienvenido {username}!! :D"}
+                return render(request,"index.html",context=context)
+            
+        form = AuthenticationForm()
+        return render(request,"users/login.html",{"error":"Formulario invalido","form":form})
+    
+    elif request.method == "GET":
+        form = AuthenticationForm()
+    return render(request,"users/login.html",{"form":form})
